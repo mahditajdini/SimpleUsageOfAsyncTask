e3a79e4049dc7aab96aa20d3e71ad4f510794d6d
Mahdi tajdini <mahditajdini99@gmail.com>
2023-11-15 21:43:32 +0330

    making CallApi calss

--- app/src/main/java/com/the_tj/simpleusageofasynctask/MainActivity.kt
+++ app/src/main/java/com/the_tj/simpleusageofasynctask/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.the_tj.simpleusageofasynctask
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private inner class CallApi() : AsyncTask<Any, Void, String>() {
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result: String
+            var connection: HttpURLConnection? = null
+            try {
+                val url = URL("https://run.mocky.io/v3/a883d7c6-d399-4d61-bb96-14749c05be0f")
+                connection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.doOutput = true
+
+                val httpResult: Int = connection.responseCode
+                if (httpResult == HttpURLConnection.HTTP_OK) {
+                    val inputStream = connection.inputStream
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+                    try {
+                        while (reader.readLine().also { line = it } != null) {
+                            stringBuilder.append(line+"\n")
+                        }
+                    }catch (e:IOException){
+                        e.printStackTrace()
+                    }finally {
+                        try {
+
+                        }catch (){
+
+                        }
+                    }
+
+
+                }
+
+            }
+        }
     }
 }
