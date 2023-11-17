2aff33cc87cc80193abb9104d1768d54255845c3
Mahdi tajdini <mahditajdini99@gmail.com>
2023-11-17 19:45:10 +0330

    make json visible in textview

--- app/src/main/java/com/the_tj/simpleusageofasynctask/MainActivity.kt
+++ app/src/main/java/com/the_tj/simpleusageofasynctask/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.the_tj.simpleusageofasynctask
 
+import android.app.Dialog
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.the_tj.simpleusageofasynctask.databinding.ActivityMainBinding
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private val Tag="MYTAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        CallApi().execute()
     }
 
     private inner class CallApi() : AsyncTask<Any, Void, String>() {
+        private lateinit var customProgressDialog:Dialog
 
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
         override fun doInBackground(vararg params: Any?): String {
             var result: String
             var connection: HttpURLConnection? = null
@@ -41,16 +54,43 @@ class MainActivity : AppCompatActivity() {
                         e.printStackTrace()
                     }finally {
                         try {
+                            inputStream.close()
 
-                        }catch (){
+                        }catch (e:IOException){
+                            e.printStackTrace()
 
                         }
                     }
 
-
+                    result=stringBuilder.toString()
+                }else{
+                    result=connection.responseMessage
                 }
 
+            }catch (e:SocketTimeoutException){
+                result="Connection Time out"
+            } finally {
+                connection?.disconnect()
             }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            binding.tv.text=result.toString()
+
+            Log.i(Tag,result.toString())
+
+        }
+
+        private fun showProgressDialog(){
+            customProgressDialog= Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.custom_progress)
+            customProgressDialog.show()
+        }
+        private fun cancelProgressDialog(){
+            customProgressDialog.dismiss()
         }
     }
 }
